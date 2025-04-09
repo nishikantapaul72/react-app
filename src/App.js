3576853309cd252c7d16703b9212831a34545412
Nishi Kanta Paul <nishi.kanta@brainstation-23.com>
2025-04-09 11:18:48 +0600

    Implement Form component with controlled inputs and submission handling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import ClockList from "./components/ClockList";
+import Form from "./components/Form";
 
 function App() {
   const quantities = [1, 2, 3];
   return (
     <div>
-      <ClockList quantities={quantities} />
+      <Form />
     </div>
   );
 }
