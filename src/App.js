bba382522808507b11f9cc6164defe67f5b4ca41
Nishi Kanta Paul <nishi.kanta@brainstation-23.com>
2025-04-10 15:24:56 +0600

    implement Todo component with input handling and state management

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from "react";
-import ClickCounter from "./components/ClickCounter";
-import Counter from "./components/Counter";
-import Section from "./components/Section";
-import ThemeContext from "./contexts/themeContext";
-import { render } from "@testing-library/react";
+import Todo from "./components/Todo";
 
 export default class App extends React.Component {
-  state = {
-    theme: "light",
-    switchTheme: () => {
-      this.setState(({ theme }) => {
-        if (theme === "dark") {
-          return {
-            theme: "light",
-          };
-        }
-        return {
-          theme: "dark",
-        };
-      });
-    },
-  };
-
   render() {
     return (
-      <div className="app">
-        <Counter>
-          {/* This is a render prop  */}
-          {(counter, incrementCount) => (
-            <ClickCounter count={counter} incrementCount={incrementCount} />
-          )}
-        </Counter>
-        <ThemeContext.Provider value={this.state}>
-          <Section />
-        </ThemeContext.Provider>
+      <div>
+        <Todo />
       </div>
     );
   }
